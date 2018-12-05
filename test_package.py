240502419f36706a7f3de895774b4bb2a9610c2d
StraetenMax <maxstraeten@yahoo.com>
2018-12-05 08:06:22 +0100

    réalisation de test avec try et except

--- test_package.py
+++ test_package.py
@@ -1,2 +1,17 @@
-from package.fonctions import table
-table(5) # Appel de la fonction table
+
+try:
+    import package.fonctions
+    fonctions.table(5) # Appel de la fonction table
+
+except NameError:
+    print("La variable n'a pas été définie.")
+
+except TypeError:
+    print("La variable possède un type incompatible avec la division.")
+
+except ZeroDivisionError:
+    print("La variable est égale à 0.")
+
+else:    
+    from package.fonctions import table
+    table(5) # Appel de la fonction table
